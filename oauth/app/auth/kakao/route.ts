362354dd0f495308231eb8736bc9508804d7cfaa
codingquokka <104996819+l1lsang@users.noreply.github.com>
2026-01-26 15:31:30 +0900

    dasf

---
+++ oauth/app/auth/kakao/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
+
+  return NextResponse.json({
+    ok: true,
+    code,
+  });
+}
